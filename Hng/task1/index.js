1f5ee5146dd783e4fc963d2cb143aef194e4d088
Bratipah <bratipahmh@gmail.com>
2022-10-27 20:31:32 +0300

    stringfy data to JSON

--- Hng/task1/index.js
+++ Hng/task1/index.js
@@ -8,15 +8,18 @@ const app = express();
 app.listen(3000, ()=> console.log("Application server is running"));
 
 app.get("/",(req,res)=>{
+
+    //API data
     const slackUsername = "Bratipah Kioko";
     const backend = true;
     const age = 22;
-    const bio = "I'm a mern-stack developer with an interest of upscaling my backend skiils for an upcoming job opportunity";
-    res.json({
-        slackUsername: (slackUsername),
-        backend: (backend),
-        age: (age),
-        bio: (bio)
-    })
-    app.use();
+    const bio = "I'm a mern-stack developer with an interest of upscaling my backend skills for an upcoming job opportunity";
+    
+    //Stringy data to JSON format
+    res.send(JSON.stringify({
+        slackUsername,
+        backend,
+        age,
+        bio
+    }))
 });
